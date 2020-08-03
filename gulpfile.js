fd7c388868f6a78f7150bb39987f2a9d52fa9bfe
Slavko Vuletic <slavko.vuletic92@gmail.com>
2020-08-03 18:05:46 +0200

    gulp watch to run all exports from scss to css to min

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
-
+var sass = require('gulp-sass');
 
 //Single file export all
 gulp.task('min-css-export-single-file', function(cb){
@@ -33,7 +33,14 @@ gulp.task('min-css-export-all', function(cb){
     cb();
 });
 
+gulp.task('sass-export', function(){
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('src/css'))
+});
+
 gulp.task('watch', function(){
+    gulp.watch('src/scss/**/*.scss', gulp.series(['sass-export'])); 
     gulp.watch('src/css/*.css', gulp.series(['min-css-export-all','min-css-export-single-file'])); 
     // Other watchers
   })
